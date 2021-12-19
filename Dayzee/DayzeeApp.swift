439656db73b62e97749a4170a6208cfe7ef29155
Valerian <valerianmars@gmail.com>
2021-12-19 22:44:48 +0100

    First UI done (navigation bar)

--- Dayzee/DayzeeApp.swift
+++ Dayzee/DayzeeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DayzeeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ListView()
         }
     }
 }
---
+++ Dayzee/Views/ListView.swift
@@ -0,0 +1,54 @@
+//
+//  ListView.swift
+//  Dayzee
+//
+//  Created by VLR on 19/12/21.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                Text("Text")
+                Text("text")
+                Text("Text")
+                Text("Text")
+                Text("Text")
+                Text("Text")
+                Text("Text")
+                Text("Text")
+            }
+            
+            .navigationBarItems(leading:
+                HStack(spacing: 118) {
+                    Button(action: {
+                        print("Left button pressed")
+                    }) {
+                        Image(systemName: "arrowtriangle.left")
+                            .foregroundColor(Color.purple)
+                    }
+                    
+                    Button("Today") {
+                        print("About button pressed")
+                    }.foregroundColor(Color.black)
+                    
+                    
+                    Button(action: {
+                        print("Right button pressed")
+                    }) {
+                        Image(systemName: "arrowtriangle.right")
+                            .foregroundColor(Color.purple)
+                    }
+                }
+            )
+        }
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}

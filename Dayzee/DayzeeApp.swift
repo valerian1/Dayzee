dd13ee8ab1d38920dd2b585d47863b0bdfd69021
Valerian <valerianmars@gmail.com>
2021-12-19 16:56:05 +0100

    Initial Commit

---
+++ Dayzee/DayzeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayzeeApp.swift
+//  Dayzee
+//
+//  Created by VLR on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DayzeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
